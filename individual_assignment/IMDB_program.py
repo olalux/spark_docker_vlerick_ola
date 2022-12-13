1dbe1a2c8268443bb9bcf8a983717122639b01e6
olalux <dziurdzia.o@gmail.com>
2022-12-13 13:55:59 +0000

    renamed file

--- individual_assignment/IMDB_program.py
+++ individual_assignment/IMDB_program.py
@@ -1,8 +1,9 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
-BUCKET = ""
-KEY = ""
+BUCKET = "dmacademy-course-assets"
+file_before = "vlerick/pre_release.csv"
+file_after = "vlerick/after_release.csv"
 
 config = {
     "spark.jars.python": "com.amazonaws.auth.instanceprofilecredentialsprovider"
